112b88a9a91412df6aeca1276262c6cd32a501d4
lucarrr <Andrea.lucaroni@live.com>
2025-11-16 15:06:44 +0100

    [feat] added morphing feature to graph functions

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -1,15 +1,29 @@
 using UnityEngine;
+using static FunctionLibrary;
 
 public class Graph : MonoBehaviour
 {
     [SerializeField]
     FunctionLibrary.FunctionName function;
 
+    public enum TransitionMode { Cycle, Random }
+
+    [SerializeField]
+    TransitionMode transitionMode;
+
+
     public int resolution = 50;
     [SerializeField]
     Transform pointPrefab;
 
     Transform[] points;
+
+    [SerializeField, Min(0f)]
+    float functionDuration = 1f, transitionDuration = 1f;
+    float duration;
+    bool transitioning;
+
+    FunctionLibrary.FunctionName transitionFunction;
 
     void Awake()
     {
@@ -28,10 +42,37 @@ public class Graph : MonoBehaviour
 
     
     void Update()
-    {   
-        FunctionLibrary.Function f= FunctionLibrary.GetFunction(function);
+    {
+        duration += Time.deltaTime;
+        if (transitioning) {
+            if (duration >= transitionDuration)
+            {
+                duration -= transitionDuration;
+                transitioning = false;
+            }
+        }
+        else if (duration >= functionDuration)
+        {
+            duration -= functionDuration;
+            transitioning = true;
+            transitionFunction = function;
+            PickNextFunction();
+        }
+        if (transitioning)
+        {
+            UpdateFunctionTransition();
+        }
+        else
+        {
+            UpdateFunction();
+        }
+    }
+
+    void UpdateFunction()
+    {
+        FunctionLibrary.Function f = FunctionLibrary.GetFunction(function);
         float time = Time.time;
-        float step= 2f/resolution;
+        float step = 2f / resolution;
         float v = 0.5f * step - 1f;
         for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++)
         {
@@ -41,11 +82,40 @@ public class Graph : MonoBehaviour
                 z += 1;
                 v = (z + 0.5f) * step - 1f;
             }
-            //Transform point = points[i];
-            //Vector3 position = point.localPosition; 
-            //position.y = f(position.x, position.z, time);
-            float u = (x+0.5f) * step -1f;
-            points[i].localPosition = f(u,v,time);
+            float u = (x + 0.5f) * step - 1f;
+            points[i].localPosition = f(u, v, time);
         }
     }
+
+    void UpdateFunctionTransition()
+    {
+        FunctionLibrary.Function 
+            from = FunctionLibrary.GetFunction(transitionFunction),
+            to = FunctionLibrary.GetFunction(function);
+        float progress = duration / transitionDuration;
+        float time = Time.time;
+        float step = 2f / resolution;
+        float v = 0.5f * step - 1f;
+        for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++)
+        {
+            if (x == resolution)
+            {
+                x = 0;
+                z += 1;
+                v = (z + 0.5f) * step - 1f;
+            }
+            float u = (x + 0.5f) * step - 1f;
+            points[i].localPosition = FunctionLibrary.Morph(
+                u, v, time, from, to, progress
+            ); ;
+        }
+    }
+
+    void PickNextFunction()
+    {
+        function = transitionMode == TransitionMode.Cycle ?
+            FunctionLibrary.GetNextFunctionName(function) :
+            FunctionLibrary.GetRandomFunctionNameOtherThan(function);
+    }
+
 }
